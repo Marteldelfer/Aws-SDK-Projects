94b45990d20df3d6a8f2f427cde3d1036872f2c5
Murillo Ferraz <marteldelfer@gmail.com>
2024-09-30 12:41:25 -0300

    setup springboot project

---
+++ sdk1/src/test/java/com/martel/sdk1/Sdk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martel.sdk1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
