005a8faf055589c5cc29e68592b96443dc01aa8e
Murillo Ferraz <marteldelfer@gmail.com>
2024-10-01 23:30:20 -0300

    create project

---
+++ sdk2/src/test/java/com/martel/sdk2/Sdk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martel.sdk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
